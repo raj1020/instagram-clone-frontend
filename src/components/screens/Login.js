361c154963b043a28a9ee3fb24ff6aeeaf50484b
raj1020 <raj.1020@hotmail.com>
2020-09-02 14:06:42 +1000

    created login and signup page UI

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
 
 const Login = ()  => {
         return (
-            <h1>Login</h1>
+                <div className = "mycard">
+                    <div className = "card auth-card input-field">
+                        <h2>Instagram</h2>
+                        <input 
+                            type= "text"
+                            placeholder = "email"
+                        />
+                        <input 
+                            type= "text"
+                            placeholder = "password"
+                        />
+                        <button className="btn waves-effect waves-light #64b5f6 blue lighten-2"> 
+                            Login
+                        </button>
+                        <div>
+                            <Link to='/signup'> Don't have an account?</Link>
+                        </div>
+                       
+
+                    </div>
+                   
+                </div>
         )
 };
 
