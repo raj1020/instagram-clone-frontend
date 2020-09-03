d12c2b9ddac0f5331204c8414116aa54682edc50
raj1020 <raj.1020@hotmail.com>
2020-09-03 11:53:34 +1000

    created UI for homepage

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -2,7 +2,44 @@ import React from 'react';
 
 const Home = ()  => {
         return (
-            <h1>Home</h1>
+           <div className="home">
+               <div className="card home-card">
+                   <h5>Sam Rana</h5>
+                   <div className="card-image">
+                        <img src="https://images.unsplash.com/photo-1592669707702-bf1af6569f17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="woman with cigar" />
+                   </div>
+                   <div className="card-content">
+                   <i class="material-icons" style={{color:"red"}}>favorite</i>
+                       <h6>title</h6>
+                       <p>this is amazing post</p>
+                       <input type="text" placeholder="add a comment" />
+                   </div>
+               </div>
+               <div className="card home-card">
+                   <h5>Sam Rana</h5>
+                   <div className="card-image">
+                        <img src="https://images.unsplash.com/photo-1592669707702-bf1af6569f17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="woman with cigar" />
+                   </div>
+                   <div className="card-content">
+                   <i class="material-icons" style={{color:"red"}}>favorite</i>
+                       <h6>title</h6>
+                       <p>this is amazing post</p>
+                       <input type="text" placeholder="add a comment" />
+                   </div>
+               </div>
+               <div className="card home-card">
+                   <h5>Sam Rana</h5>
+                   <div className="card-image">
+                        <img src="https://images.unsplash.com/photo-1592669707702-bf1af6569f17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="woman with cigar" />
+                   </div>
+                   <div className="card-content">
+                   <i class="material-icons" style={{color:"red"}}>favorite</i>
+                       <h6>title</h6>
+                       <p>this is amazing post</p>
+                       <input type="text" placeholder="add a comment" />
+                   </div>
+               </div>
+           </div>
         )
 };
 
