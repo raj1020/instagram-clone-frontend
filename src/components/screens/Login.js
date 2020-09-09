90f8ef856a60e56d2c99ad357eacfdf7884cea0b
raj1020 <raj.1020@hotmail.com>
2020-09-09 12:38:33 +1000

    used local storage for persisting data after login, used useEffect hook for saving image url in cloudinary and to send the url to the backend and rectified errors

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -29,6 +29,8 @@ const Login = ()  => {
             if(data.error) {
                 M.toast({html: data.error, classes: "#c62828 red darken-3"});
             } else {
+                localStorage.setItem("jwt", data.token);
+                localStorage.setItem("user", JSON.stringify( data.user));
                 M.toast({html: "logged in successfully", classes: "#43a047 green darken-1"});
                 history.push('/');
             }
