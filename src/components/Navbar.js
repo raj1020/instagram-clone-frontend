5c40ca79ab2661247167fef6131c250065209003
raj1020 <raj.1020@hotmail.com>
2020-09-26 15:57:11 +1000

    added logout functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,28 @@
 import React, { useContext } from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import { UserContext } from '../App';
 
 
 const NavBar = () => {
 
     const {state, dispatch} = useContext(UserContext);
-   
+    const history = useHistory();
     const renderList = () => {
       if(state) {
         return (<div>
                   <li><Link to="profile">Profile</Link></li>
                   <li><Link to="create">CreatePost</Link></li>
+                  <button className="btn  #c62828 red darken-3"
+                            onClick={() => {
+                              localStorage.clear();
+                              dispatch({type: "CLEAR"})
+                              history.push("/login");
+                            }}
+                        >
+
+                            Log Out
+                        </button>
+
         </div>)
       } else {
         return (
