84e0466a65830bd1430601593489d920a1a0a4ae
raj1020 <raj.1020@hotmail.com>
2020-10-14 13:42:25 +1100

    modified UI for the users to upload their profile pic and display default pic if profile not available

--- src/components/screens/Signup.js
+++ src/components/screens/Signup.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link, useHistory} from 'react-router-dom';
 import M from 'materialize-css';
 
@@ -10,8 +10,35 @@ const Signup = ()  => {
         const [name, setName] = useState("");
         const [password, setPassword] = useState("");
         const [email, setEmail]  = useState("");
+        const [image, setImage] = useState("");
+        const [url, setUrl] = useState(undefined);
 
-        const PostData = () => {
+        useEffect(() => {
+            if(url) {
+                uploadFields();
+            }
+        }, [url])
+
+        const uploadPic = () => {
+            const data = new FormData();
+            data.append("file", image);
+            data.append("upload_preset", "insta-clone");
+            data.append("cloud_name", "pradthecoder");
+            fetch("	https://api.cloudinary.com/v1_1/pradthecoder/image/upload", {
+                method: "post",
+                body: data
+            })
+            .then(res => res.json())
+            .then(data => {
+                setUrl(data.url);
+            }).catch(err => {
+                console.log(err)
+            })
+    
+        }
+
+        const uploadFields = () => {
+
             if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
                 M.toast({html: "Invalid Email", classes: "#c62828 red darken-3"});
                 return
@@ -24,7 +51,8 @@ const Signup = ()  => {
                 body: JSON.stringify({
                     name,
                     password,
-                    email
+                    email,
+                    pic: url
                 })
             }).then(res => res.json())
             .then(data => {
@@ -37,6 +65,16 @@ const Signup = ()  => {
             }).catch(err => {
                 console.log(err);
             })
+
+        }
+
+        const PostData = () => {
+            if(image) {
+                uploadPic()
+            } else {
+                uploadFields()
+            }
+            
         }
 
         return (
@@ -61,6 +99,20 @@ const Signup = ()  => {
                             value = {password}
                             onChange = {(e) => setPassword(e.target.value)  }
                         />
+
+                        <div className="file-field input-field">
+                            <div className="btn #64b5f6 blue darken-1">
+                                <span>Upload Image</span>
+                                <input type="file"
+
+                                    onChange = {(e) => {setImage(e.target.files[0]);}}
+                                />
+                            </div>
+                            <div className="file-path-wrapper">
+                                <input className="file-path validate" type="text"/>
+                            </div>
+                        </div>
+
                         <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
                             onClick={() => PostData()}
                         >
