587427ccd53e9705839cbc88d7b076db6c5f2c86
raj1020 <raj.1020@hotmail.com>
2020-10-14 15:28:35 +1100

    added an action on reducer for updating profile pic and updated UI for updating profile pic

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -5,6 +5,8 @@ const Profile = ()  => {
 
     const [mypics, setPics] =useState([]);
     const {state, dispatch} =  useContext(UserContext);
+    const [image, setImage] = useState("");
+    const [url, setUrl] = useState(undefined);
 
     useEffect(() => {
         fetch('/mypost', {
@@ -20,13 +22,46 @@ const Profile = ()  => {
        
     }, [])
 
+    useEffect(() => {
+        if(image) {
+            const data = new FormData();
+            data.append("file", image);
+            data.append("upload_preset", "insta-clone");
+            data.append("cloud_name", "pradthecoder");
+            fetch("	https://api.cloudinary.com/v1_1/pradthecoder/image/upload", {
+                method: "post",
+                body: data
+            })
+            .then(res => res.json())
+            .then(data => {
+                setUrl(data.url);
+                localStorage.setItem("user", JSON.stringify({...state, pic: data.url}))
+                dispatch({type:"UPDATEPIC", payload: data.url})
+                window.location.reload()
+            }).catch(err => {
+                console.log(err)
+            })
+
+        }
+
+    }, [image])
+
+    const updatePhoto = (file) => {
+        setImage(file)
+        
+    }
+
         return (
             <div style = {{maxWidth: "550px", margin:"0px auto"}}>
                 <div style= {{
-                    display: "flex",
-                    justifyContent: "space-around",
                     margin: "18px 0px",
                     borderBottom: "1px solid grey"
+                }}>
+                
+                <div style= {{
+                    display: "flex",
+                    justifyContent: "space-around",
+                   
                 }}>
                     <div>
                         <img style = {{width: "160px", height: "160px", borderRadius: "80px"}}
@@ -44,6 +79,21 @@ const Profile = ()  => {
                             
                         </div>
                     </div>
+                </div>
+                <div className="file-field input-field" style={{margin: "10px"}}>
+                            <div className="btn #64b5f6 blue darken-1">
+                                <span>Update Image</span>
+                                <input type="file"
+
+                                    onChange = {(e) => {updatePhoto(e.target.files[0]);}}
+                                />
+                            </div>
+                            <div className="file-path-wrapper">
+                                <input className="file-path validate" type="text"/>
+                            </div>
+                        </div>
+
+                       
                 </div>
 
 
