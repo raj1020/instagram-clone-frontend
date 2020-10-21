60b5582e0add3e4663259f35b89e579113736036
raj1020 <raj.1020@hotmail.com>
2020-10-21 14:08:33 +1100

    added search field on the nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useEffect, useState } from 'react';
 import {Link, useHistory} from 'react-router-dom';
 import { UserContext } from '../App';
+import M from 'materialize-css';
 
 
 const NavBar = () => {
 
+    const searchModal = useRef(null)
+    const [search, setSearch] = useState('')
     const {state, dispatch} = useContext(UserContext);
     const history = useHistory();
+
+  useEffect(() => {
+    M.Modal.init(searchModal.current);
+    
+  }, [])
+
     const renderList = () => {
       if(state) {
         return (<div>
-                  <li><Link to="/profile">Profile</Link></li>
-                  <li><Link to="/create">CreatePost</Link></li>
-                  <li><Link to="/postsifollow">Posts I follow</Link></li>
+                  <li key = "1"><i data-target="modal1" className="material-icons modal-trigger" style = {{color: "black"}}>search</i></li>
+                  <li key = "2"><Link to="/profile">Profile</Link></li>
+                  <li key = "3"><Link to="/create">CreatePost</Link></li>
+                  <li key = "4"><Link to="/postsifollow">Posts I follow</Link></li>
                   <button className="btn  #c62828 red darken-3"
                             onClick={() => {
                               localStorage.clear();
@@ -43,6 +53,33 @@ const NavBar = () => {
             {renderList()}
           </ul>
         </div>
+      
+        <div id="modal1" className="modal" ref ={searchModal} style ={{color: "black"}}>
+          <div className="modal-content">
+
+            <input 
+                type= "text"
+                placeholder = "Search Users"
+                value = {search}
+                onChange = {(e) => setSearch(e.target.value)  }
+            />
+            <ul className="collection">
+              <li className="collection-item">Alvin</li>
+              <li className="collection-item">Alvin</li>
+              <li className="collection-item">Alvin</li>
+              <li className="collection-item">Alvin</li>
+              <li className="collection-item">Alvin</li>
+              <li className="collection-item">Alvin</li>
+              <li className="collection-item">Alvin</li>
+              <li className="collection-item">Alvin</li>
+              <li className="collection-item">Alvin</li>
+            </ul>
+          </div>
+          <div className="modal-footer">
+            <a  className="modal-close waves-effect waves-green btn-flat">Agree</a>
+          </div>
+        </div>
+          
       </nav>
     );
 }
